0432d582455a06a2a9a1285922f960f5b5f7a998
KEHP-K <rnjsxovnd@gmail.com>
2021-01-25 18:58:36 +0900

    refactor

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,14 @@ router.get('/new',function(req,res,next){
   res.render('new');
 });
 
-router.post('/create', async function(req,res,next){
+router.get('/:id/edit', async function(req,res,next){
+  const user=await model.User.findOne({
+    where:{id:req.params.id}
+  });
+  res.render('edit', {id:user.id, username:user.username, email:user.email});
+})
+
+router.post('/', async function(req,res,next){
   const plain=req.body.password;
   const salt=bcrypt.genSaltSync(10);
   const hashPassword=bcrypt.hashSync(plain, salt);
@@ -25,5 +32,24 @@ router.post('/create', async function(req,res,next){
   console.log(user);
   res.redirect('/');
 });
+
+router.put('/:id', async function(req,res,next){
+  const plain=req.body.password;
+  const salt=bcrypt.genSaltSync(10);
+  const hashPassword=bcrypt.hashSync(plain, salt);
+  await model.User.update({
+    username:req.body.username,
+    email:req.body.email,
+    password:hashPassword
+  },{where:{id:req.params.id}});
+  res.redirect('/');
+})
+
+router.delete('/:id', async function(req,res,next){
+  await model.User.destroy({
+    where:{id:req.params.id}
+  });
+  res.redirect('/');
+})
 
 module.exports = router;
